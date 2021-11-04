f129c134b3e3e7b06d0343983f937c144d36aa8e
harodggg <haroldtiansheng@gmail.com>
2021-11-04 11:46:32 +0800

    update

--- cli/src/lib.rs
+++ cli/src/lib.rs
@@ -1,23 +1,39 @@
 use std::io::Error as error;
-
-use std::path::PathBuf;
 use structopt::StructOpt;
+
+#[derive(StructOpt,Debug)]
+#[structopt(about = "Select a subcommand")]
+enum SubComand{
+    #[structopt(name = "testnet")]
+    Testnet { 
+    },
+
+    #[structopt(name = "btc")]
+    Btc {
+       
+    },
+
+    #[structopt(name = "regtest")]
+    Regtest { 
+    },
+
+    #[structopt(name = "import")]
+    Import {
+        import: String,
+    },
+
+    #[structopt(name = "rollback")]
+    Rollback { 
+        rollback: String,
+    }
+
+}
 
 #[derive(Debug, StructOpt)]
 #[structopt(name = "rbitcoin", about = "A rust client for bitcoin")]
 struct Opt {
-    /// Activate debug mode
-    #[structopt(short = "d", long = "debug")]
-    debug: bool,
-    /// Set speed
-    #[structopt(short = "s", long = "speed", default_value = "42")]
-    speed: f64,
-    /// Input file
-    #[structopt(parse(from_os_str))]
-    input: PathBuf,
-    /// Output file, stdout if not present
-    #[structopt(parse(from_os_str))]
-    output: Option<PathBuf>,
+    #[structopt(subcommand)]
+    subcommand: SubComand,
 }
 
 pub fn run() -> Result<(),error> { 
