e0c1cdc58eb861481ed3060acef09321d7a6c826
harodggg <haroldtiansheng@gmail.com>
2022-01-28 23:23:13 +0800

    add fn[init,strftime] for log

--- log/src/lib.rs
+++ log/src/lib.rs
@@ -1,9 +1,34 @@
+use ansi_term::*;
+use chrono::prelude::*;
+use std::env;
+use env_logger::{Builder, Target};
+
+fn strftime() -> String { 
+    Local::now().format("%Y-%m-%d %H:%M:%S").to_string()
+}
+
+
+pub fn init() -> () {
+    let mut builder = Builder::from_default_env();
+
+    builder.target(Target::Stdout);
+
+    println!("{}",strftime());
+
+    ()
+
+}
+
+
 
 #[cfg(test)]
 mod tests {
+    use super::*;    
     #[test]
     fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+        init();
+
     }
+
+
 }
